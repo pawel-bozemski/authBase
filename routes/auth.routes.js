af345824f852cf7d8609f25d525e4632b010c20b
Pawel Bozemski <paw.boz@gmail.com>
2020-04-14 17:59:19 +0200

    Add routes and add login name and avatar

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,10 +5,15 @@ const router = express.Router();
 router.get('/google',
   passport.authenticate('google', { scope: ['email', 'profile'] }));
 
-router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
+router.get('/callback', passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
   (req, res) => {
-    res.redirect('/user/logged');
+  res.redirect('/user/logged');
   }
 );
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
 
 module.exports = router;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/logged', (req, res) => {
-  res.render('logged');
+  // console.log('req.user', req.user)
+  req.user ? res.render('logged', {
+    username: req.user.displayName,
+    avatar: req.user.photos[0].value,
+  }) : res.redirect('/no-permission');
+
+});
+
+router.get('/profile', (req, res) => {
+  req.user ? res.render('profile') : res.redirect('/no-permission');
+});
+
+router.get('/profile/settings', (req, res) => {
+  req.user ? res.render('profileSettings') : res.redirect('/no-permission');
 });
 
 router.get('/no-permission', (req, res) => {
